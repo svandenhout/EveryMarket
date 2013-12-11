16b639885358927a6bd34ce113b64fab59cbbfa9
svandenhout <stevenvandenhout@gmail.com>
2013-12-11 11:56:23 +0100

    fixed front-end signup system, now i just need to fix it's controller

--- js/signup.js
+++ js/signup.js
@@ -1,60 +1,72 @@
 $(document).ready(function() {
+    
+    // TODO: i should just treat these like member vars next time
     var geoCoder = new google.maps.Geocoder();
-    var myLatlng = google.maps.LatLng(52.094590, 5.438232);
+    
+    // nederland centraal latlng
+    var myLatlng = new google.maps.LatLng(52.094590, 5.438232);
     
     // Get some values from elements on the page:
     var form = $("#sign-up-form"),
+        url = form.attr('action'),
         name = form.find("input[name='name']"),
         email = form.find("input[name='email']"),
         password = form.find("input[name='password']"),
         user_address = form.find("input[name='user-address']"),
         geocode_address = form.find("input[name='geocode-address']"),
         location = form.find("input[name='location']"),
-        submit = form.find("input[name='submit']"),
-        url = form.attr("action");
+        submit = form.find("input[name='submit']");
+        
     
     var mapOptions = {
-            zoom: 3,
+            zoom: 6,
             center: myLatlng
     };
     
-    var map = new google.maps.Map(document.getElementById('address-map'), mapOptions)
+    var map = new google.maps.Map(
+        document.getElementById('address-map'), mapOptions
+    );
     
     $("#address-button").click(function() {
-        console.log(user_address.val());
-        geoCoder.geocode( { 'address': user_address.val()}, function(results, status) {
-            console.log(results)
+        geoCoder.geocode({ 'address': user_address.val()}, function(results, status) {
+            myLatlng = results[0].geometry.location;
             geocode_address.val(results[0].formatted_address);
-            location.val(results[0].geometry.location);
+            location.val(results[0].geometry.location.toString());
+            
+            // show marker of current position
+            new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: 'Do you live here?'
+            });
+            
+            // change center and zoom in on the user address
+            map.panTo(myLatlng);
+            map.setZoom(12);
             submit.show();
-        })
-        
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: 'Do you live here?'
         });
     });
     
-        
     $("#sign-up-form").submit(function(event) {
         // Stop form from submitting normally
         event.preventDefault();
         
+        console.log(url);
         // Send the data using post
         var posting = $.post( 
             url, { 
                 name: name.val(), 
                 email: email.val(), 
                 password: password.val(),
-                location: location.val() 
-            } 
+                location: location.val()
+            }
         );
         
         // Put the results in a div
         posting.done(function(data) {
+            console.log("HALLO");
             console.log(data);
             $( "#sign-up-status" ).innerHTML = data;
         });
     });
-})
+});
